398d8571f0c9a16b4cca87258382dd3c3f458225
fbryant92 <forrestcbryant@gmail.com>
2022-05-06 19:22:56 -0700

    finish exam

--- test_2.py
+++ test_2.py
@@ -42,3 +42,23 @@ def calculate_percent_above_average(grades, avg):
     return (count * 100) / len(grades)
 
 """
+def calculate_percent_above_average(grades, avg):
+    count = 0
+    for grade in grades:
+        if grade > avg:
+            count += 1
+    return (count * 100) / len(grades)
+def main():
+    f = open('Final.txt')
+    grades = []
+    for line in f:
+        grades.append(int(line.strip()))
+    print("Number of grades:", len(grades))
+    avg = 0
+    for grade in grades:
+        avg += grade
+    avg /= len(grades)
+    print("Average grade:", avg)
+    print("Percentage of grades above average: {:.2f}%".format(calculate_percent_above_average(grades, avg)))
+    f.close()
+
