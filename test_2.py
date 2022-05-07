a96513e5dcda42d60f9c93dc7c8d7fe1a6d2194d
fbryant92 <forrestcbryant@gmail.com>
2022-05-06 19:12:34 -0700

    pseudo-code complete

--- test_2.py
+++ test_2.py
@@ -14,3 +14,31 @@ Number of Grades: 24
 Average Grade: 83.25
 Percentage of grades above average: 54.17%
 """
+
+"""
+#Main Function
+def main():
+    f = open('Final.txt')
+    grades = []
+    for line in f:
+        grades.append(int(line.strip()))
+    print("Number of grades:", len(grades))
+    avg = 0
+    for grade in grades:
+        avg += grade
+    avg /= len(grades)
+    print("Average grade:", avg)
+    print("Percentage of grades above average: {:.2f}%".format(calculate_percent_above_average(grades, avg)))
+    f.close()
+
+
+main()
+#calculate_percent_above_average function
+def calculate_percent_above_average(grades, avg):
+    count = 0
+    for grade in grades:
+        if grade > avg:
+            count += 1
+    return (count * 100) / len(grades)
+
+"""
